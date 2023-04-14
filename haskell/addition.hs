8c2e607db70560a4250b3ccda420f1e025c98bd5
subodh kamble <39451041+tusuii@users.noreply.github.com>
2023-04-14 22:37:35 +0530

    haskell basic problems and code
    basic of haskell and problems and code solutions

---
+++ haskell/addition.hs
@@ -0,0 +1,7 @@
+main = do
+ putStrLn"Enter first number"
+ num1 <- getLine
+ putStrLn"Enter Second number"
+ num2 <- getLine
+ let sum = (read num1 :: Int) + (read num2 :: Int)
+ putStrLn("Sum : " ++ show sum)
