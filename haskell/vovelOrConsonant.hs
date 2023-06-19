797352eb5b52d03d5115b9079f1351d877925754
subodh kamble <39451041+tusuii@users.noreply.github.com>
2023-06-19 22:36:36 +0530

    Create vovelOrConsonant.hs

---
+++ haskell/vovelOrConsonant.hs
@@ -0,0 +1,24 @@
+isVowel :: Char -> Bool
+isVowel c = c `elem` "aeiouAEIOU"
+
+main :: IO ()
+main = do
+  putStrLn "Enter a character:"
+  input <- getLine
+  let character = head input
+  if isVowel character
+    then putStrLn "The character is a vowel."
+    else putStrLn "The character is a consonant."
+
+{-
+*Main> main
+Enter a character:
+c
+The character is a consonant.
+
+*Main> main
+Enter a character:
+a
+The character is a vowel.
+
+-}
